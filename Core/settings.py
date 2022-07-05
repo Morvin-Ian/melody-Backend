2a71f0e44e9b8c2c12ca9f24c77b2672d6b3c577
Morvin-Ian <morvinian@gmail.com>
2022-07-05 10:04:59 +0300

    finalize

--- Core/settings.py
+++ Core/settings.py
@@ -136,11 +136,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://melody-talk.vercel.app/"
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "https://melody-talk.vercel.app/"
 
-]
+# ]
 
 CORS_ALLOW_ALL_ORIGINS= True
 
